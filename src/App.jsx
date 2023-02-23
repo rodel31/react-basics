5d87969b9c0893a8b326eb6e050b626209e56ee7
Rodel Ramos <84778672+rodel31@users.noreply.github.com>
2023-02-23 09:48:42 +0800

    modify App.css and add button to Card

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,9 @@ import './App.css'
 
 import Card from './Card'
 const App = () => {
-    const [count, setCount] = useState(0)
     const [user, setUser] = useState()
     const [password, setPassword] = useState()
 
-    const increment = () => {
-        setCount(count + 1)
-    }
 
     const handleOnChangeUser = (e) =>{
         setUser(e.target.value)
@@ -21,13 +17,13 @@ const App = () => {
 
     return(
         <>
-            <div>Count: {count}</div>
-            <button onClick={increment}>Like</button>
             <form action="">
                 <label>User: <input type="text" onChange={handleOnChangeUser} value={user} /></label>
                 <label>Password: <input type="password" onChange={handleOnChangePassword} value={password} /></label>
+                <button type='submit'>Login</button>
             </form>
             <div className='main'>
+                <h1>User's Card</h1>
                 <Card title="Software Engineer" name="Rod-Invi" age={31} gender="Male"/>
                 <Card title="Frontend Developer" name="Rod-design" age={30} gender="Male"/>
                 <Card title="Backend Developer" name="Rod-occassion" age={3} gender="Female"/>
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import React from "react";
 
 const Card = ({title,name, age, gender})=> {
+
+const [count,setCount]=useState(0)
+
+const increment = () =>{
+    setCount(count + 1)
+}
 
     return(
         <>
@@ -9,6 +16,10 @@ const Card = ({title,name, age, gender})=> {
                 <p>Name: {name}</p>
                 <p>Age: {age}</p>
                 <p>Gender: {gender}</p>
+                <div>
+                    <button onClick={increment}>Like</button>
+                    <p>{count}</p>
+                </div>
             </div>
         </>
     )
